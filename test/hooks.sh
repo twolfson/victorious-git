59178b199142b3ce2306f0fbb0bf61684fadfe79
Todd Wolfson <todd@twolfson.com>
2015-08-13 01:38:47 -0500

    Simplified our afplay.out

--- test/hooks.sh
+++ test/hooks.sh
@@ -45,20 +45,38 @@ fixture_git_init --template "$repo_dir/dotgit"
   git checkout HEAD~1 &> /dev/null; git checkout -b dev/conflicting.branch &> /dev/null
   echo "wurld" > world.txt; git add world.txt; git commit -m "Added wurld file" > /dev/null
   #   Perform our merge, declare this branch the victor, and commit with default text
-  export VICTORIOUS_GIT_AFPLAY_CONTENT="merge-conflict-success"
   git merge master &> /dev/null || true; git checkout HEAD -- world.txt; git commit --no-edit &> /dev/null
   # Wait for afplay.out to be written due to forking
   sleep 0.1
 
     # it plays our victory music upon success
-    if test "$(cat afplay.out)" != "merge-conflict-success"; then
+    if ! test -f afplay.out; then
       echo "\`victorious-git\` did not play upon merge commit resolution" 1>&2
       exit 1
     fi
 
-  # when we commit with a merge conflict but the commit fails (e.g. due to lack of a commit message)
-    # it does not play our victory music
+# A git directory initialized with victorious-git
+fixture_git_init --template "$repo_dir/dotgit"
 
+  # when we commit with a merge conflict but the commit fails (e.g. due to lack of a commit message)
+  echo "hello" > hello.txt; git add hello.txt; git commit -m "Added hello file" > /dev/null
+  echo "world" > world.txt; git add world.txt; git commit -m "Added world file" > /dev/null
+  #   Add conflicting "world" file on alternative branch
+  git checkout HEAD~1 &> /dev/null; git checkout -b dev/conflicting.branch &> /dev/null
+  echo "wurld" > world.txt; git add world.txt; git commit -m "Added wurld file" > /dev/null
+  #   Perform our merge, declare this branch the victor, and commit with default text
+  git merge master &> /dev/null || true; git checkout HEAD -- world.txt; git commit --no-edit &> /dev/null
+  # Wait for afplay.out to be written due to forking
+  sleep 0.1
+
+    # it does not play our victory music
+    if test -f afplay.out; then
+      echo "\`victorious-git\` played music upon failing to write a merge commit" 1>&2
+      exit 1
+    fi
+
+# A git directory initialized with victorious-git
+fixture_git_init --template "$repo_dir/dotgit"
   # when we commit with a merge conflict but the commit fails and we make a non-conflicting commit
     # it does not play our victory music
 
