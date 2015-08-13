315fd41285cbff0033fb476fe323600529901c68
Todd Wolfson <todd@twolfson.com>
2015-08-13 02:37:41 -0500

    Added assertion for 1.8

--- bin/travis-setup.sh
+++ bin/travis-setup.sh
@@ -7,7 +7,14 @@ set -x
 if test "$GIT_VERSION" = "1.7"; then
   # TODO: Assert the git version is correct
 # Otherwise, if we are using git@1.8, then do nothing
-  # TODO: Assert the git version is correct
+elif test "$GIT_VERSION" = "1.8"; then
+  # Assert the git version is correct (e.g. "git version 1.8.5.6")
+  # TODO: Test assertion failures all at the end
+  git_version="$(git --version)"
+  if ! (echo "$git_version" | grep -E "^git version 1.8"); then
+    echo "Expected \`git --version\` to be \"1.8\" but it was \"$git_version\"" 1>&2
+    exit 1
+  fi
 # Otherwise, if we are using git@2.x, then remove the existing git and install `git` via new PPA
   # TODO: Assert the git version is correct
 # Otherwise, complain about an unrecognized git
