2870cfa5d320020c1b7277b68dd2f5fa70eec928
Todd Wolfson <todd@twolfson.com>
2015-08-13 01:57:20 -0500

    Removed redundant setups

--- test/hooks.sh
+++ test/hooks.sh
@@ -13,6 +13,15 @@ fixture_git_init() {
   git init $* > /dev/null
 }
 
+add_hello_world_commits() {
+  #   Add "hello" and "world" files on `master` in 2 consecutive commits
+  echo "hello" > hello.txt; git add hello.txt; git commit -m "Added hello file" > /dev/null
+  echo "world" > world.txt; git add world.txt; git commit -m "Added world file" > /dev/null
+  #   Add conflicting "world" file on alternative branch
+  git checkout HEAD~1 &> /dev/null; git checkout -b dev/conflicting.branch &> /dev/null
+  echo "wurld" > world.txt; git add world.txt; git commit -m "Added wurld file" > /dev/null
+}
+
 # Prepend our with a custom "afplay" for testing
 export PATH="$test_dir/test-files/bin:$PATH"
 
@@ -38,15 +47,11 @@ fixture_git_init --template "$repo_dir/dotgit"
 fixture_git_init --template "$repo_dir/dotgit"
 
   # when we commit with a merge conflict
-  #   Add "hello" and "world" files on `master` in 2 consecutive commits
-  echo "hello" > hello.txt; git add hello.txt; git commit -m "Added hello file" > /dev/null
-  echo "world" > world.txt; git add world.txt; git commit -m "Added world file" > /dev/null
-  #   Add conflicting "world" file on alternative branch
-  git checkout HEAD~1 &> /dev/null; git checkout -b dev/conflicting.branch &> /dev/null
-  echo "wurld" > world.txt; git add world.txt; git commit -m "Added wurld file" > /dev/null
+  #   Generate our conflicting commits
+  add_hello_world_commits
   #   Perform our merge, declare this branch the victor, and commit with default text
   git merge master &> /dev/null || true; git checkout HEAD -- world.txt; git commit --no-edit &> /dev/null
-  # Wait for afplay.out to be written due to forking
+  #   Wait for afplay.out to be written due to forking
   sleep 0.1
 
     # it plays our victory music upon success
@@ -59,13 +64,10 @@ fixture_git_init --template "$repo_dir/dotgit"
 fixture_git_init --template "$repo_dir/dotgit"
 
   # when we commit with a merge conflict but the commit fails (e.g. due to lack of a commit message)
-  echo "hello" > hello.txt; git add hello.txt; git commit -m "Added hello file" > /dev/null
-  echo "world" > world.txt; git add world.txt; git commit -m "Added world file" > /dev/null
-  git checkout HEAD~1 &> /dev/null; git checkout -b dev/conflicting.branch &> /dev/null
-  echo "wurld" > world.txt; git add world.txt; git commit -m "Added wurld file" > /dev/null
+  add_hello_world_commits
   #   Force an empty commit message via `cat /dev/null` (empty string) piped to stdin of `git commit -F -` (use stdin as message)
   git merge master &> /dev/null || true; git checkout HEAD -- world.txt; cat /dev/null | git commit -F - &> /dev/null || true
-  # Wait for afplay.out to be written due to forking
+  #   Wait for afplay.out to be written due to forking
   sleep 0.1
 
     # it does not play our victory music
@@ -77,16 +79,13 @@ fixture_git_init --template "$repo_dir/dotgit"
 # A git directory initialized with victorious-git
 fixture_git_init --template "$repo_dir/dotgit"
   # when we commit with a merge conflict but the commit fails and we make a non-conflicting commit
-  echo "hello" > hello.txt; git add hello.txt; git commit -m "Added hello file" > /dev/null
-  echo "world" > world.txt; git add world.txt; git commit -m "Added world file" > /dev/null
-  git checkout HEAD~1 &> /dev/null; git checkout -b dev/conflicting.branch &> /dev/null
-  echo "wurld" > world.txt; git add world.txt; git commit -m "Added wurld file" > /dev/null
+  add_hello_world_commits
   git merge master &> /dev/null || true; git checkout HEAD -- world.txt; cat /dev/null | git commit -F - &> /dev/null || true
   #  Exit our merge state
   git reset --hard &> /dev/null; git checkout master &> /dev/null
   #  Create our non-conflicting commit
   echo "moon" > moon.txt; git add moon.txt; git commit -m "Added moon file" > /dev/null
-  # Wait for afplay.out to be written due to forking
+  #   Wait for afplay.out to be written due to forking
   sleep 0.1
 
     # it does not play our victory music
