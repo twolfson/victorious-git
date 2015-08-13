ebc549840424d8b2b2abd14b42f1ab25f09156af
Todd Wolfson <todd@twolfson.com>
2015-08-13 02:34:59 -0500

    Added recognized/unrecognized versions

--- bin/travis-setup.sh
+++ bin/travis-setup.sh
@@ -3,13 +3,15 @@
 set -e
 set -x
 
-# Get our current `git version`
-git --version
-
 # If we are using git@1.7, then remove the existing git and install the Ubuntu default
+if test "$GIT_VERSION" = "1.7"; then
   # TODO: Assert the git version is correct
 # Otherwise, if we are using git@1.8, then do nothing
   # TODO: Assert the git version is correct
 # Otherwise, if we are using git@2.x, then remove the existing git and install `git` via new PPA
   # TODO: Assert the git version is correct
 # Otherwise, complain about an unrecognized git
+else
+  echo "Unrecognized GIT_VERSION \"$GIT_VERSION\". Please either remove it or define it via \`travis-setup.sh\`" 1>&2
+  exit 1
+fi
