799b37cd9226ae15f9cdbe0dac8497c2a71e1d4b
Todd Wolfson <todd@twolfson.com>
2015-08-13 00:45:21 -0500

    Generated a test

--- test/index.sh
+++ test/index.sh
@@ -3,11 +3,21 @@
 set -e
 
 # Resolve our test and repo locations
-TEST_DIR="$(dirname "${BASH_SOURCE[0]}")"
-REPO_DIR="$TEST_DIR/.."
-echo "$TEST_DIR"
-echo "$REPO_DIR"
+test_dir="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+repo_dir="$(dirname "$test_dir")"
 
+# Define test helpers
+fixture_git_init() {
+  tmp_dir="$(mktemp -d)"
+  cd "$tmp_dir"
+  git init $* > /dev/null
+}
 
 # A git directory initialized with victorious-git
-  # git init
+fixture_git_init --template "$repo_dir/dotgit"
+
+  # has our fixtures
+  if ! ls .git/hooks/post-commit || ! ls .git/hooks/pre-commit; then
+    echo "\`git\` folder initialized with our template doesn't have our hooks" 1>&2
+    exit 1
+  fi
