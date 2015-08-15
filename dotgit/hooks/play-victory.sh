dd27b1556b2430cb222747dbb26df0390d90ab51
Todd Wolfson <todd@twolfson.com>
2015-08-15 03:51:35 -0500

    Added another abstraction

---
+++ dotgit/hooks/play-victory.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Exit upon the first failing command
+set -e
+
+# Resolve our parameters
+filepath="$1"
+if test "$filepath" = ""; then
+  echo "No filepath was specified for \`$0\`" 1>&2
+  echo "Usage: $0 [filepath]" 1>&2
+  exit
+fi
+
+# Determine which player is supported and play from it
+if which afplay &> /dev/null; then
+  afplay -v 5 "$filepath" &> /dev/null
+elif which mplayer &> /dev/null; then
+  mplayer "$filepath" &> /dev/null
+else
+  echo "No player found for \`victory.mp3\` playback =(" 1>&2
+  exit 1
+fi
