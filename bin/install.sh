12b7a371edac5bc767dbd5bd370a7bbbf251672f
Todd Wolfson <todd@twolfson.com>
2015-08-15 04:05:58 -0500

    Jamming on install.sh

---
+++ bin/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Exit upon first failure
+set -e
+
+# If our template directory already exists, explain and error out
+dotgit_source="$PWD/dotgit/"
+dotgit_target="~/.config/victorious-git/dotgit/"
+if test -d "$dotgit_target"; then
+  echo "There is an existing installation of \`victorious-git\`." 1>&2
+  echo "To prevent unwanted deletions, please remove or backup your existing install:" 1>&2
+  echo "# rm -r \"$dotgit_target\"" 1>&2
+  exit 1
+fi
+
+# Copy over our dotgit
+set -x
+set +x
+
+# Download our music
+./install-music.sh "$dotgit_target/hooks/"
+
+# Set up our global templatedir
+set -x
+set +x
+
+# Notify the user of success
+echo "victorious-git has been successfully installed"
+
+# Play our victory music
+"$dotgit_target/hooks/play-victory.sh" "$dotgit_target/hooks/victory.mp3" &
