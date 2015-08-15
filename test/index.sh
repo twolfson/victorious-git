31d75dc6fc02e7f61aa1f1152aa05155e21a22c3
Todd Wolfson <todd@twolfson.com>
2015-08-15 03:01:42 -0500

    Moved to running multiple scripts

--- test/index.sh
+++ test/index.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 # Run our tests and verify each has no output
-./test/hooks.sh &> test/hooks.out
-exit_code="$?"
-if test "$exit_code" != "0"; then
-  echo "hooks.sh had non-zero exit code \"$exit_code\". Here's its output:" 1>&2
-  cat test/hooks.out
-  exit 1
-fi
-if test "$(cat test/hooks.out)" != ""; then
-  echo "hooks.sh had non-empty output. Here's its output:" 1>&2
-  cat test/hooks.out
-  exit 1
-fi
+for test_script in ls test/{hooks.sh,install.sh}; do
+  out_filepath="$test_script.out"
+  "$test_script" &> "$out_filepath"
+  exit_code="$?"
+  if test "$exit_code" != "0"; then
+    echo "$test_script had non-zero exit code \"$exit_code\". Here's its output:" 1>&2
+    cat "$out_filepath"
+    exit 1
+  fi
+  if test "$(cat $out_filepath)" != ""; then
+    echo "$test_script had non-empty output. Here's its output:" 1>&2
+    cat "$out_filepath"
+    exit 1
+  fi
+done
 
 # Output our success
 echo "All tests are passing" 1>&2
